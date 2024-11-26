429c941d776a29a9c9d07db90f21b4211a15c295
webloginid <92395834+webloginid@users.noreply.github.com>
2024-11-26 16:00:44 -0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
         userPoolId: 'us-east-1_m5TQGWxTs', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: 'eir84tsjicj00e720fmcjcr2l', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientId: '7kco8cis903l11vpukb30euhu3', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
