3b8d0d583484af6280551aaafb66010e42cd16ea
webloginid <92395834+webloginid@users.noreply.github.com>
2024-12-02 16:37:04 -0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_WOW4d4qtb', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '573jte61j1kiha07buf9par8jv', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-1_Ge7W4lgf1', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '4nv3tl6amn48mrtd5jcdrju1d3', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
